322d2b6a6307b1116a28dbf6796bf78cc7d7c41a
cvbiard <cvbiard@hotmail.com>
2020-12-02 17:57:36 -0800

    shit man

--- Clark Biard/PA7/header.h
+++ Clark Biard/PA7/header.h
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define INIT_CARD  0, 0 ,-1, "string", "string"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,13 +10,15 @@ struct card
 	int face;
 	int suit;
 	int id;
+	char* faces;
+	char* suits;
 };
 struct hand
 {
 	struct card array[5];
 };
 
-void shuffle(int wDeck[][13]);
-void deal(const int wDeck[][13], const char* wFace[], const char* wSuit[]);
-void init_cards(struct card cars[52]);
-void print_cards(struct card cards[52], const char* wFace[], const char* wSuit[]);
+void shuffle(struct card wDeck[52], struct card cards[52]);
+void deal(struct card wDeck[52], struct card hand[5], struct card dummy, int deckpos);
+void init_cards(struct card cars[52], const char* wFace[], const char* wSuit[]);
+void print_cards(struct card cards[52]);
