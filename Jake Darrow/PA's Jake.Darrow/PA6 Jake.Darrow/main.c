018d9c09d8e92489dd16efdff4e1a454e133bb9e
cvbiard <cvbiard@hotmail.com>
2020-12-07 19:09:06 -0800

    Revert "Nun"
    This reverts commit 17ba66b2b6280e328a1c54103b1c5ca43c7d99ee.

--- Jake Darrow/PA's Jake.Darrow/PA6 Jake.Darrow/main.c
+++ Jake Darrow/PA's Jake.Darrow/PA6 Jake.Darrow/main.c
@@ -2,124 +2,16 @@
 
 int main(void)
 {
-	FILE* output = fopen("battleship.log", "w");
-	srand((int)time(NULL));
-	int turn, counter = 0;
+	char board[MAX_ROWS][MAX_COLS] = { {'\0'} };
+	srand(time(NULL)); //Random based on time
 
-	struct player player;
-	player.id = 0;
-	
-	player.board = malloc(MAX_ROWS * sizeof(char*)); // sets up 1st dimension
-	for (int i = 0; i < 10; i++)
-	{
-		player.board[i] = malloc(MAX_COLS * sizeof(char*)); // sets up 2nd dimension
-	}
-	player.hits = 0;
-	player.misses = 0;
-	player.shots = 0;
-	player.ratio = 0.0;
-
-	struct player bot;
-	bot.id = 1;
-
-	bot.board = malloc(MAX_ROWS * sizeof(char*)); // sets up 1st dimension
-
-	for (int i = 0; i < 10; i++)
-	{
-		bot.board[i] = malloc(MAX_COLS * sizeof(char*)); // sets up 2nd dimension
-	}
-
-	bot.hits = 0;
-	bot.misses = 0;
-	bot.shots = 0;
-	bot.ratio = 0.0;
-
-	struct ship destroyer; //wonderful wonderful structs
-	destroyer.size = 2;
-	destroyer.letter = 'd';
-	destroyer.name = "destroyer";
-	destroyer.hitsp1 = 0;
-	destroyer.hitsp2 = 0;
-	destroyer.sunkp1 = 0;
-	destroyer.sunkp2 = 0;
-
-	struct ship submarine;
-	submarine.size = 3;
-	submarine.letter = 's';
-	submarine.name = "submarine";
-	submarine.hitsp1 = 0;
-	submarine.hitsp2 = 0;
-	submarine.sunkp1 = 0;
-	submarine.sunkp2 = 0;
+	game_menu();
+	pick_turns();
+	set_ships();
 
 
-	struct ship cruiser;
-	cruiser.size = 3;
-	cruiser.letter = 'r';
-	cruiser.name = "cruiser";
-	cruiser.hitsp1 = 0;
-	cruiser.hitsp2 = 0;
-	cruiser.sunkp1 = 0;
-	cruiser.sunkp2 = 0;
+	init_board(board, MAX_ROWS, MAX_COLS);
+	print_board(board, MAX_ROWS, MAX_COLS);
 
-	struct ship battleship;
-	battleship.size = 4;
-	battleship.letter = 'b';
-	battleship.name = "battleship";
-	battleship.hitsp1 = 0;
-	battleship.hitsp2 = 0;
-	battleship.sunkp1 = 0;
-	battleship.sunkp2 = 0;
-
-
-	struct ship carrier;
-	carrier.size = 5;
-	carrier.letter = 'c';
-	carrier.name = "carrier";
-	carrier.hitsp1 = 0;
-	carrier.hitsp2 = 0;
-	carrier.sunkp1 = 0;
-	carrier.sunkp2 = 0;
-
-	struct ship ships[5] = { destroyer, submarine, cruiser, battleship, carrier };
-	struct player playa[2] = { player, bot }; //putting structs in arrays to run through for loops is so sweet
-
-	game_menu();		//Very proud of main function, fun to look over whole gameplay like this
-	turn = pick_turns();
-	init_board(player);
-	init_board(bot);
-	bot_ships(bot, ships);
-	print_board(player, 0);
-	player_ships(player, ships);
-
-	while (check_winner(playa, ships) == 0)
-	{
-		if (turn == 0)
-		{
-			system("cls");
-			print_board(bot, 0);
-			print_board(player, 0);
-			pick_shot(player, playa, ships, 1, counter, output);
-			system("PAUSE");
-			system("cls");
-		}
-		if (turn == 1)
-		{
-			system("cls");
-			print_board(bot, 0);
-			print_board(player, 0);
-			pick_shot(bot, playa, ships, 0, counter, output);
-			system("PAUSE");
-			system("cls");
-			print_board(bot, 0);
-			print_board(player, 0);
-			pick_shot(player, playa, ships, 1, counter, output);
-			system("PAUSE");
-			system("cls");
-		}
-		counter = counter + 1;
-	}
-	print_stats(playa, output);
-	fclose(output);
 	return 0;
 }
